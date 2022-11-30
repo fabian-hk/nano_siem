e9525432c12eca1328fe42b621c326d9e32380a4
asadhasan73 <113350302+asadhasan73@users.noreply.github.com>
2022-11-30 14:37:18 +0200

    Update __init__.py

--- cron/__init__.py
+++ cron/__init__.py
@@ -1,14 +1,23 @@
 def is_tor_exit_node(input):
     import urllib
-    import urllib.request
-    import numpy as np
-    import pandas as pd
-    df = pd.read_csv('/home/vagrant/work/projects/chipwhisperer/jupyter/TORIPS.csv', header = None)
-    N= len(df)
-    for i in range(N):
-        if df.values[i] == input:
-            output = 1
-            break
-        else:
-            output = 0
-        return output
+    import datetime
+    import os
+    
+    #check if file exits
+    if os.path.isfile("/home/vagrant/work/projects/chipwhisperer/tor_exit_nodes.txt"):
+        my_file = open("/home/vagrant/work/projects/chipwhisperer/tor_exit_nodes.txt", "r")
+        modified_date = datetime.datetime.fromtimestamp(os.path.getmtime('tor_exit_nodes.txt'))
+        duration = today - modified_date
+        # download new file and replace only if exits more than a day
+        if duration.days > 1:
+            urllib.request.urlretrieve("https://check.torproject.org/torbulkexitlist", "tor_exit_nodes.txt")
+            my_file = open("tor_exit_nodes.txt", "r")
+    else:
+        my_file = open("/home/vagrant/work/projects/chipwhisperer/tor_exit_nodes.txt", "r")
+    data = my_file.read()
+    data_into_list = data.split("\n")
+    my_file.close()
+    if input in data_into_list:
+        return True
+    else:
+        return False
