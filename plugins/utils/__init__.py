35d0a49a739ca935fdfa42b5f7ba031c7654fb3d
Hasan Sarwar <98755767+hsw95@users.noreply.github.com>
2022-11-30 17:18:24 +0200

    Update __init__.py
    The function to check TOR IP has been added

--- plugins/utils/__init__.py
+++ plugins/utils/__init__.py
@@ -54,5 +54,26 @@ def ip_to_coordinates(input: str) -> Tuple[float, float, str, str, str]:
         return None, None, None, None, None
 
 
-def is_tor_exit_node(ip: str) -> bool:
-    pass
+def is_tor_exit_node(input):
+    import urllib
+    import datetime
+    import os
+    
+    #check if file exists
+    if os.path.isfile("/home/vagrant/work/projects/chipwhisperer/tor_exit_nodes.txt"):
+        my_file = open("/home/vagrant/work/projects/chipwhisperer/tor_exit_nodes.txt", "r")
+        modified_date = datetime.datetime.fromtimestamp(os.path.getmtime('tor_exit_nodes.txt'))
+        duration = today - modified_date
+        # download new file and replace only if exists more than a day
+        if duration.days > 1:
+            urllib.request.urlretrieve("https://check.torproject.org/torbulkexitlist", "tor_exit_nodes.txt")
+            my_file = open("tor_exit_nodes.txt", "r")
+    else:
+        my_file = open("/home/vagrant/work/projects/chipwhisperer/tor_exit_nodes.txt", "r")
+    data = my_file.read()
+    data_into_list = data.split("\n")
+    my_file.close()
+    if input in data_into_list:
+        return True
+    else:
+        return False
